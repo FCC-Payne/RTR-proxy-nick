263ac0447d80004c7406dc88f108f82c393704aa
Nicholas Petrik <frokrab@gmail.com>
2018-06-14 12:11:33 -0700

    Set up proxy to render page with component placeholders

--- proxy/server.js
+++ proxy/server.js
@@ -3,10 +3,22 @@ const express = require('express');
 const app = express();
 
 app.get('/photo-viewer', (req, res) => {
-  console.log('request received!')
   res.redirect('http://localhost:3001/photo-viewer');
 });
 
+app.get('/forms', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('/carousel', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get('/reviews', (req, res) => {
+  res.sendStatus(200);
+});
+
 app.use('/:id', express.static('public'));
+app.use('/:id', express.static('screenshots'));
 
 app.listen(3000);
